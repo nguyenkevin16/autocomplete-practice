303ff6ba8ceae4823ae3ac467b9c878a01fba300
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-05-28 14:17:05 -0700

    Add keyword suggestions logic

--- frontend/components/formula_editor.jsx
+++ frontend/components/formula_editor.jsx
@@ -5,21 +5,57 @@ class FormulaEditor extends React.Component {
     super(props);
 
     this.state = {
-      formula: ''
+      formula: '',
+      keyword: '',
+      context: '',
+      field: ''
     }
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.getContext = this.getContext.bind(this);
+    this.getChoices = this.getChoices.bind(this);
+  }
+
+  getContext(formula, pos) {
+    let startOfContext = pos - 1;
+    if (startOfContext < 0) {
+      startOfContext = 1;
+    }
+    while (startOfContext > 0) {
+      let char = formula[startOfContext];
+      if (char === " " || char === "(" || char === ")" || char === ",") {
+        startOfContext++;
+        break;
+      }
+      startOfContext--;
+    }
+    let endOfContext = pos;
+    return formula.substring(startOfContext, endOfContext);
+  }
+
+  getChoices(context) {
+    if (!context || context === "") {
+      return [];
+    }
+    
+    const keywords = ['sum', 'min', 'avg', 'max', 'count', '@revenue', '@quantity', '@created_at'];  
+    return keywords.filter(function(choice){ return choice.indexOf(context) !== -1 });
   }
 
   handleChange(event) {
     event.preventDefault();
-    this.setState({ formula: event.target.value });
+
+    let context = this.getContext(event.target.value, event.target.value.slice(0, event.selectionStart).length)
+    let suggestions = this.getChoices(context);
+    
+    this.setState({ 
+      formula: event.target.value,
+    });
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    alert('A formula was submitted: ' + this.state.formula);
     console.log('A formula was submitted: ' + this.state.formula);
   }
 
